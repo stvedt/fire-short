3d6b82a50456fec89acd701aaa86fecbb0bbd42e
Stephen Tvedt <stvedt@zola.com>
2018-10-16 23:08:44 -0400

    newer version of firebase tools and functions and node/react living happily together

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,13 @@
 const functions = require('firebase-functions');
+const express = require('express');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+var app = express();
+
+app.get('/test', (req, res) => {
+  res.send('test');
+})
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.app = functions.https.onRequest(app);
